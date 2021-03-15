46f76c5f342176acf57a79e3bce7452a384e103c
arno <lingquan.liu@quvideo.com>
2021-03-15 16:45:57 +0800

    [learn-web mvc]第三次重构

---
+++ tiny-monitor/src/main/java/com/arno/learn/grow/tiny/monitor/bean/processor/ModelMBeanCreator.java
@@ -0,0 +1,45 @@
+package com.arno.learn.grow.tiny.monitor.bean.processor;
+
+import javax.management.MBeanException;
+import javax.management.modelmbean.ModelMBeanInfo;
+import javax.management.modelmbean.RequiredModelMBean;
+
+/**
+ * @Author: lingquan.liu@quvideo.com
+ * @Date: 2021/3/15 16:19
+ * @Description:
+ */
+public class ModelMBeanCreator {
+
+    public enum ManagedResource {
+        MANAGED_OBJ("ObjectReference"),
+        MANAGED_HANDLE("Handle"),
+        MANAGED_IOR("IOR"),
+        MANAGED_EJB("EJBHandle"),
+        MANAGED_RMI("RMIReference"),
+        ;
+        private final String value;
+
+        ManagedResource(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+
+    public static RequiredModelMBean creatorModelBean(Object object, ManagedResource managedResource) throws Exception {
+        RequiredModelMBean modelMBean = new RequiredModelMBean();
+        modelMBean.setManagedResource(object, managedResource.value);
+        ModelMBeanInfo info = assembleMBeanInfo(object);
+        modelMBean.setModelMBeanInfo(info);
+        return modelMBean;
+    }
+
+    private static ModelMBeanInfo assembleMBeanInfo(Object object) {
+
+        return null;
+    }
+
+}
